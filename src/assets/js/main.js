59bc2def0577b0b05e4596b6c9a334491d869a31
Nicolás Capdet <ncapdetm@gmail.com>
2024-07-02 03:06:48 -0500

    refactor: :recycle: change "let" to "const" where necessary

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -17,7 +17,7 @@
    * Easy event listener function
    */
   const on = (type, el, listener, all = false) => {
-    let selectEl = select(el, all);
+    const selectEl = select(el, all);
 
     if (selectEl) {
       if (all) {
@@ -54,14 +54,14 @@
     "click",
     "#navbar .nav-link",
     function (e) {
-      let section = select(this.hash);
+      const section = select(this.hash);
       if (section) {
         e.preventDefault();
 
-        let navbar = select("#navbar");
-        let header = select("#header");
-        let sections = select("section", true);
-        let navlinks = select("#navbar .nav-link", true);
+        const navbar = select("#navbar");
+        const header = select("#header");
+        const sections = select("section", true);
+        const navlinks = select("#navbar .nav-link", true);
 
         navlinks.forEach((item) => {
           item.classList.remove("active");
@@ -71,7 +71,7 @@
 
         if (navbar.classList.contains("navbar-mobile")) {
           navbar.classList.remove("navbar-mobile");
-          let navbarToggle = select(".mobile-nav-toggle");
+          const navbarToggle = select(".mobile-nav-toggle");
           navbarToggle.classList.toggle("bi-list");
           navbarToggle.classList.toggle("bi-x");
         }
@@ -110,11 +110,11 @@
    */
   window.addEventListener("load", () => {
     if (window.location.hash) {
-      let initial_nav = select(window.location.hash);
+      const initial_nav = select(window.location.hash);
 
       if (initial_nav) {
-        let header = select("#header");
-        let navlinks = select("#navbar .nav-link", true);
+        const header = select("#header");
+        const navlinks = select("#navbar .nav-link", true);
 
         header.classList.add("header-top");
 
@@ -138,13 +138,13 @@
   /**
    * Skills animation
    */
-  let skilsContent = select(".skills-content");
+  const skilsContent = select(".skills-content");
   if (skilsContent) {
     new Waypoint({
       element: skilsContent,
       offset: "80%",
       handler: function (direction) {
-        let progress = select(".progress .progress-bar", true);
+        const progress = select(".progress .progress-bar", true);
         progress.forEach((el) => {
           el.style.width = el.getAttribute("aria-valuenow") + "%";
         });
@@ -185,14 +185,14 @@
    * Porfolio isotope and filter
    */
   window.addEventListener("load", () => {
-    let portfolioContainer = select(".portfolio-container");
+    const portfolioContainer = select(".portfolio-container");
     if (portfolioContainer) {
-      let portfolioIsotope = new Isotope(portfolioContainer, {
+      const portfolioIsotope = new Isotope(portfolioContainer, {
         itemSelector: ".portfolio-item",
         layoutMode: "fitRows",
       });
 
-      let portfolioFilters = select("#portfolio-flters li", true);
+      const portfolioFilters = select("#portfolio-flters li", true);
 
       on(
         "click",
