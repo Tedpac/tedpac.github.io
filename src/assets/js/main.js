8483b41d8064234228720c1fab2f378bc7787194
Nicolás Capdet <ncapdetm@gmail.com>
2024-07-02 23:06:32 -0500

    feat: :sparkles: navigate to the header when the name link is clicked

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -8,3 +8,9 @@ const animationManager = new AnimationManager();
 animationManager.startAnimation();
 
 initializeEmailAddressHandler();
+
+// Navigate to the header when the name link is clicked.
+document.getElementById("name-link").addEventListener("click", (event) => {
+  event.preventDefault();
+  document.getElementById("header-link").click();
+});
