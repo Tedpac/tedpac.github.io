5bb59f108d2a95e2ba7665367ce026e18ccde633
Nicolás Capdet <ncapdetm@gmail.com>
2024-07-04 21:44:19 -0500

    chore: :technologist: configure the Webpack development server to reload on HTML changes

--- webpack/dev.js
+++ webpack/dev.js
@@ -1,10 +1,12 @@
 const { merge } = require("webpack-merge");
 const common = require("./common.js");
 
+// @ts-ignore
 module.exports = merge(common, {
   mode: "development",
   devServer: {
     port: 9000,
+    watchFiles: ["./src/**/*.html"],
   },
   devtool: "eval-source-map",
 });
--- webpack/prod.js
+++ webpack/prod.js
@@ -1,6 +1,7 @@
 const { merge } = require("webpack-merge");
 const common = require("./common.js");
 
+// @ts-ignore
 module.exports = merge(common, {
   mode: "production",
   devtool: "source-map",
