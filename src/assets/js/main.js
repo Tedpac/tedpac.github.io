779e32050046bbc9183c0a542b6c8c70e4ebb8a5
Nicolás Capdet <ncapdetm@gmail.com>
2024-07-05 06:33:15 -0500

    feat: :sparkles: update information

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -14,3 +14,9 @@ document.getElementById("name-link").addEventListener("click", (event) => {
   event.preventDefault();
   document.getElementById("header-link").click();
 });
+
+// Navigate to the contact section when the "contact me" link is clicked.
+document.getElementById("contact-me-link").addEventListener("click", (event) => {
+  event.preventDefault();
+  document.getElementById("contact-link").click();
+});
